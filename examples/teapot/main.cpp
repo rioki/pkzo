7e52346e04bf36217c9db6bc03fc2738af6c6780
Sean Farrell <sean.farrell@rioki.org>
2015-12-26 13:35:01 +0100

    Migrates teapot example to glt.

--- examples/teapot/main.cpp
+++ examples/teapot/main.cpp
@@ -3,7 +3,7 @@
 #include <pkzo.h>
 
 #include <gl/glew.h>
-#include "glm.h"
+#include <glt.h>
 
 int main(int argc, char* argv[])
 {
@@ -46,17 +46,17 @@ int main(int argc, char* argv[])
     pkzo::Texture steel_plate;
     steel_plate.load("SteelPlate_Diffuse.png");
   
-    glmPerspective(projection, 45.0f, 800.0f/600.0f, 0.1f, 100.0f);
+    gltPerspective(projection, 45.0f, 800.0f/600.0f, 0.1f, 100.0f);
     
     window.on_draw([&] () {
         
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glEnable(GL_DEPTH_TEST);
         
-        glmLoadIdentity(modelview);
-        glmLookAt(modelview, 5, 5, 5, 0, 0, 0, 0, 0, 1);
-        glmRotate(modelview, ax, 0, 1, 0);
-        glmRotate(modelview, ay, 1, 0, 0);        
+        gltLoadIdentity(modelview);
+        gltLookAt(modelview, 5, 5, 5, 0, 0, 0, 0, 0, 1);
+        gltRotate(modelview, ax, 0, 1, 0);
+        gltRotate(modelview, ay, 1, 0, 0);        
 
         shader.bind();
         shader.set_uniform_matrix("uProjection", projection, 16);
