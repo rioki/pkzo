38248afd5cb29564b20f64d7757badaa0d5e1065
Sean Farrell <sean.farrell@rioki.org>
2016-01-17 13:14:11 +0100

    Increses light.

--- examples/atrium/AtriumScene.cpp
+++ examples/atrium/AtriumScene.cpp
@@ -12,10 +12,10 @@ namespace atrium
 {
     AtriumScene::AtriumScene(pkzo::Library& lib)
     {
-        ambient_light.set_color(Color(0.05f, 0.05f, 0.05f));
+        ambient_light.set_color(Color(0.1f, 0.1f, 0.1f));
         add_node(ambient_light);
 
-        sun_light.set_color(Color(0.5f, 0.5f, 0.5f));
+        sun_light.set_color(Color(0.7f, 0.7f, 0.7f));
         sun_light.set_orientation(pkzo::Quaternion::axis_angle(90.0f, pkzo::Vector3(0, 0, 1)) * 
                                   pkzo::Quaternion::axis_angle(20, pkzo::Vector3(1, 0, 0)));
         add_node(sun_light);
