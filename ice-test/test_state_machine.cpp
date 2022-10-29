1e6ed35ba3695b45193b0084c3821265999366c1
Sean Farrell <sean.farrell@rioki.org>
2022-10-29 18:20:06 +0200

    Add implementation of state machine.

---
+++ ice-test/test_state_machine.cpp
@@ -0,0 +1,223 @@
+// State Machine
+// Copyright 2022 Sean Farrell <sean.farrell@rioki.org>
+//
+// This program is free software. It comes without any warranty, to
+// the extent permitted by applicable law. You can redistribute it
+// and/or modify it under the terms of the Do What The Fuck You Want
+// To Public License, Version 2, as published by Sam Hocevar. See
+// http://www.wtfpl.net/ for more details.
+
+#include "pch.h"
+
+#include <ice/StateMachine.h>
+
+enum class State
+{
+    INIT,
+    STATE1,
+    STATE2,
+    END
+};
+
+TEST(StateMachine, create)
+{
+    auto sm = sm::StateMachine<State>(State::INIT);
+
+    EXPECT_EQ(State::INIT, sm.get_state());
+}
+
+TEST(StateMachine, create_default_zero)
+{
+    auto sm = sm::StateMachine<State>();
+
+    EXPECT_EQ(State::INIT, sm.get_state());
+}
+
+TEST(StateMachine, change_state)
+{
+    auto sm = sm::StateMachine<State>(State::INIT);
+    EXPECT_EQ(State::INIT, sm.get_state());
+
+    sm.change_state(State::STATE1);
+    EXPECT_EQ(State::STATE1, sm.get_state());
+
+    sm.change_state(State::STATE2);
+    EXPECT_EQ(State::STATE2, sm.get_state());
+
+    sm.change_state(State::END);
+    EXPECT_EQ(State::END, sm.get_state());
+}
+
+TEST(StateMachine, queue_state)
+{
+    auto sm = sm::StateMachine<State>(State::INIT);
+    EXPECT_EQ(State::INIT, sm.get_state());
+
+    sm.queue_state(State::STATE1);
+    EXPECT_EQ(State::INIT, sm.get_state());
+    sm.tick();
+    EXPECT_EQ(State::STATE1, sm.get_state());
+
+    sm.queue_state(State::STATE2);
+    EXPECT_EQ(State::STATE1, sm.get_state());
+    sm.tick();
+    EXPECT_EQ(State::STATE2, sm.get_state());
+
+    sm.queue_state(State::END);
+    EXPECT_EQ(State::STATE2, sm.get_state());
+    sm.tick();
+    EXPECT_EQ(State::END, sm.get_state());
+}
+
+TEST(StateMachine, processing_intructions)
+{
+    auto sm = sm::StateMachine<State>(State::INIT);
+
+    auto state1_tick = 0u;
+    sm.on_tick(State::STATE1, [&] () {
+        state1_tick++;
+    });
+
+    auto state2_tick = 0u;
+    sm.on_tick(State::STATE2, [&] () {
+        state2_tick++;
+    });
+
+    sm.tick();
+
+    sm.change_state(State::STATE1);
+    sm.tick();
+    sm.tick();
+
+    sm.change_state(State::STATE2);
+    sm.tick();
+    sm.tick();
+    sm.tick();
+
+    sm.change_state(State::END);
+    sm.tick();
+
+    EXPECT_EQ(State::END, sm.get_state());
+    EXPECT_EQ(2, state1_tick);
+    EXPECT_EQ(3, state2_tick);
+}
+
+TEST(StateMachine, processing_intructions_with_queue_state)
+{
+    auto sm = sm::StateMachine<State>(State::INIT);
+
+    auto state1_tick = 0u;
+    sm.on_tick(State::STATE1, [&] () {
+        state1_tick++;
+    });
+
+    auto state2_tick = 0u;
+    sm.on_tick(State::STATE2, [&] () {
+        state2_tick++;
+    });
+
+    sm.tick();
+
+    sm.queue_state(State::STATE1);
+    sm.tick();
+    sm.tick();
+
+    sm.queue_state(State::STATE2);
+    sm.tick();
+    sm.tick();
+    sm.tick();
+
+    sm.queue_state(State::END);
+    sm.tick();
+
+    EXPECT_EQ(State::END, sm.get_state());
+    EXPECT_EQ(2, state1_tick);
+    EXPECT_EQ(3, state2_tick);
+}
+
+TEST(StateMachine, pre_and_post_intructions)
+{
+    auto sm = sm::StateMachine<State>(State::INIT);
+
+    auto state1_enter = 0u;
+    sm.on_enter(State::STATE1, [&] () {
+        state1_enter++;
+    });
+    auto state1_exit = 0u;
+    sm.on_exit(State::STATE1, [&] () {
+        state1_exit++;
+    });
+
+    auto state2_enter = 0u;
+    sm.on_enter(State::STATE2, [&] () {
+        state2_enter++;
+    });
+    auto state2_exit = 0u;
+    sm.on_exit(State::STATE2, [&] () {
+        state2_exit++;
+    });
+
+    sm.tick();
+
+    sm.change_state(State::STATE1);
+    sm.tick();
+    sm.tick();
+
+    sm.change_state(State::STATE2);
+    sm.tick();
+    sm.tick();
+    sm.tick();
+
+    sm.change_state(State::END);
+    sm.tick();
+
+    EXPECT_EQ(State::END, sm.get_state());
+    EXPECT_EQ(1, state1_enter);
+    EXPECT_EQ(1, state1_exit);
+    EXPECT_EQ(1, state1_enter);
+    EXPECT_EQ(1, state1_exit);
+}
+
+TEST(StateMachine, pre_and_post_intructions_queue_state)
+{
+    auto sm = sm::StateMachine<State>(State::INIT);
+
+    auto state1_enter = 0u;
+    sm.on_enter(State::STATE1, [&] () {
+        state1_enter++;
+    });
+    auto state1_exit = 0u;
+    sm.on_exit(State::STATE1, [&] () {
+        state1_exit++;
+    });
+
+    auto state2_enter = 0u;
+    sm.on_enter(State::STATE2, [&] () {
+        state2_enter++;
+    });
+    auto state2_exit = 0u;
+    sm.on_exit(State::STATE2, [&] () {
+        state2_exit++;
+    });
+
+    sm.tick();
+
+    sm.queue_state(State::STATE1);
+    sm.tick();
+    sm.tick();
+
+    sm.queue_state(State::STATE2);
+    sm.tick();
+    sm.tick();
+    sm.tick();
+
+    sm.queue_state(State::END);
+    sm.tick();
+
+    EXPECT_EQ(State::END, sm.get_state());
+    EXPECT_EQ(1, state1_enter);
+    EXPECT_EQ(1, state1_exit);
+    EXPECT_EQ(1, state1_enter);
+    EXPECT_EQ(1, state1_exit);
+}
+
