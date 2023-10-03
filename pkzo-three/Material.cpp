b7e36b8cb45e38718dbfaa3e6df263f8c8d34be1
Sean Farrell <sean.farrell@rioki.org>
2023-10-03 20:45:47 +0200

    Add Material.

--- pkzo-three/Material.cpp
+++ pkzo-three/Material.cpp
@@ -23,6 +23,32 @@
 
 namespace pkzo::three
 {
+    std::shared_ptr<Texture> get_white_texture()
+    {
+        static auto white_texture = Texture::create_from_buffer(glm::uvec2(1, 1), ColorMode::RGBA, DataType::UINT8, "\xFF\xFF\xFF\xFF");
+        return white_texture;
+    }
+
+    std::shared_ptr<Texture> get_black_texture()
+    {
+        static auto black_texture = Texture::create_from_buffer(glm::uvec2(1, 1), ColorMode::RGBA, DataType::UINT8, "\x00\x00\x00\xFF");
+        return black_texture;
+    }
+
+    std::shared_ptr<Texture> get_normal_texture()
+    {
+        // Assuming normal is in (x,y,z) = (0, 0, 1) mapped to (r,g,b) = (128, 128, 255)
+        static auto normal_texture = Texture::create_from_buffer(glm::uvec2(1, 1), ColorMode::RGBA, DataType::UINT8, "\x80\x80\xFF\xFF");
+        return normal_texture;
+    }
+
+    std::shared_ptr<Texture> get_orange_texture()
+    {
+        // RGBA for orange is (255, 165, 0, 255)
+        static auto orange_texture = Texture::create_from_buffer(glm::uvec2(1, 1), ColorMode::RGBA, DataType::UINT8, "\xFF\xA5\x00\xFF");
+        return orange_texture;
+    }
+
     glm::vec3 rgb(unsigned int hex)
     {
         auto r = (hex >> 16) & 0xFF;
@@ -58,5 +84,125 @@ namespace pkzo::three
     glm::vec4 rgba(float r, float g, float b, float a)
     {
         return glm::vec4(r, g, b, a);
+    }
+
+    glm::vec3 Material::get_base_color_factor() const
+    {
+        return base_color_factor;
+    }
+
+    Material::Material(const glm::vec3& base_color_factor,
+                       const std::shared_ptr<Texture>& base_color_map,
+                       const std::shared_ptr<Texture>& metallic_roughness_map,
+                       const std::shared_ptr<Texture>& normal_map,
+                       const std::shared_ptr<Texture>& occlusion_map,
+                       const glm::vec3& emissive_factor,
+                       const std::shared_ptr<Texture>& emissive_map)
+    : base_color_factor(base_color_factor),
+      emissive_factor(emissive_factor),
+      base_color_map(base_color_map),
+      metallic_roughness_map(metallic_roughness_map),
+      normal_map(normal_map),
+      occlusion_map(occlusion_map),
+      emissive_map(emissive_map)
+
+    {}
+
+    void Material::set_base_color_factor(const glm::vec3& color)
+    {
+        check(glm::all(glm::greaterThanEqual(color, glm::vec3(0.0f))));
+        check(glm::all(glm::lessThanEqual(color, glm::vec3(1.0f))));
+        base_color_factor = color;
+    }
+
+    glm::vec3 Material::get_emissive_factor() const
+    {
+        return emissive_factor;
+    }
+
+    void Material::set_emissive_factor(const glm::vec3& factor)
+    {
+        check(glm::all(glm::greaterThanEqual(factor, glm::vec3(0.0f))));
+        emissive_factor = factor;
+    }
+
+    std::shared_ptr<Texture> Material::get_base_color_map() const
+    {
+        return base_color_map;
+    }
+
+    void Material::set_base_color_map(const std::shared_ptr<Texture>& value)
+    {
+        check(value != nullptr);
+        base_color_map = value;
+    }
+
+    std::shared_ptr<Texture> Material::get_metallic_roughness_map() const
+    {
+        return metallic_roughness_map;
+    }
+
+    void Material::set_metallic_roughness_map(const std::shared_ptr<Texture>& value)
+    {
+        check(value != nullptr);
+        metallic_roughness_map = value;
+    }
+
+    std::shared_ptr<Texture> Material::get_normal_map() const
+    {
+        return normal_map;
+    }
+
+    void Material::set_normal_map(const std::shared_ptr<Texture>& value)
+    {
+        check(value != nullptr);
+        normal_map = value;
+    }
+
+    std::shared_ptr<Texture> Material::get_occlusion_map() const
+    {
+        return occlusion_map;
+    }
+
+    void Material::set_occlusion_map(const std::shared_ptr<Texture>& value)
+    {
+        check(value != nullptr);
+        occlusion_map = value;
+    }
+
+    std::shared_ptr<Texture> Material::get_emissive_map() const
+    {
+        return emissive_map;
+    }
+
+    void Material::set_emissive_map(const std::shared_ptr<Texture>& value)
+    {
+        check(value != nullptr);
+        emissive_map = value;
+    }
+
+    PKZO_EXPORT std::shared_ptr<Material> create_opaque_material(const glm::vec3& base_color)
+    {
+        return std::make_shared<Material>(base_color, get_white_texture(), get_black_texture(), get_normal_texture(), get_black_texture(), glm::vec3(0.0f), get_black_texture());
+    }
+
+    PKZO_EXPORT std::shared_ptr<Material> create_opaque_material(const std::shared_ptr<Texture>& base_color, const std::shared_ptr<Texture>& metallic_roughness)
+    {
+        return std::make_shared<Material>(glm::vec3(1.0f), base_color, metallic_roughness, get_normal_texture(), get_black_texture(), glm::vec3(0.0f), get_black_texture());
+    }
+
+    PKZO_EXPORT std::shared_ptr<Material> create_opaque_material(const std::shared_ptr<Texture>& base_color, const std::shared_ptr<Texture>& metallic_roughness, const std::shared_ptr<Texture>& normal)
+    {
+        return std::make_shared<Material>(glm::vec3(1.0f), base_color, metallic_roughness, normal, get_black_texture(), glm::vec3(0.0f), get_black_texture());
+    }
+
+    PKZO_EXPORT std::shared_ptr<Material> create_emissive_material(const glm::vec3& factor)
+    {
+        return std::make_shared<Material>(glm::vec3(1.0f), get_white_texture(), get_black_texture(), get_normal_texture(), get_black_texture(), factor, get_black_texture());
+    }
+
+    PKZO_EXPORT std::shared_ptr<Material> create_emissive_material(const std::shared_ptr<Texture>& map)
+    {
+        return std::make_shared<Material>(glm::vec3(1.0f), get_white_texture(), get_black_texture(), get_normal_texture(), get_black_texture(), glm::vec3(1.0f), map);
     }
 }
--- pkzo-three/Material.h
+++ pkzo-three/Material.h
@@ -24,8 +24,34 @@
 
 #include <glm/glm.hpp>
 
+#include <pkzo/Texture.h>
+
 namespace pkzo::three
 {
+    //! Get a 1x1 white texture.
+    //!
+    //! This function returns a 1x1 white texture. The texture is cached for performance.
+    //! @return Shared pointer to the 1x1 white texture.
+    PKZO_EXPORT std::shared_ptr<Texture> get_white_texture();
+
+    //! Get a 1x1 black texture.
+    //!
+    //! This function returns a 1x1 black texture. The texture is cached for performance.
+    //! @return Shared pointer to the 1x1 black texture.
+    PKZO_EXPORT std::shared_ptr<Texture> get_black_texture();
+
+    //! Get a 1x1 normal map texture.
+    //!
+    //! This function returns a 1x1 normal map texture. The texture is cached for performance.
+    //! @return Shared pointer to the 1x1 normal map texture.
+    PKZO_EXPORT std::shared_ptr<Texture> get_normal_texture();
+
+    //! Get a 1x1 orange texture.
+    //!
+    //! This function returns a 1x1 orange texture. The texture is cached for performance.
+    //! @return Shared pointer to the 1x1 orange texture.
+    PKZO_EXPORT std::shared_ptr<Texture> get_orange_texture();
+
     //! @brief Convert RGB color from hexadecimal to glm::vec3.
     //!
     //! @param hex The RGB color in hexadecimal format (e.g., 0xFF0000 for red).
@@ -38,7 +64,7 @@ namespace pkzo::three
     //! @return The color as a glm::vec4, with each channel in [0.0, 1.0].
     PKZO_EXPORT glm::vec4 rgba(unsigned int hex);
 
-    //! @brief Convert RGB color from separate 0-255 components to glm::vec3.
+    //! Convert RGB color from separate 0-255 components to glm::vec3.
     //!
     //! @param r Red component, in [0, 255].
     //! @param g Green component, in [0, 255].
@@ -46,7 +72,7 @@ namespace pkzo::three
     //! @return The color as a glm::vec3, with each channel in [0.0, 1.0].
     PKZO_EXPORT glm::vec3 rgb(unsigned int r, unsigned int g, unsigned int b);
 
-    //! @brief Convert RGBA color from separate 0-255 components to glm::vec4.
+    //! Convert RGBA color from separate 0-255 components to glm::vec4.
     //!
     //! @param r Red component, in [0, 255].
     //! @param g Green component, in [0, 255].
@@ -55,7 +81,7 @@ namespace pkzo::three
     //! @return The color as a glm::vec4, with each channel in [0.0, 1.0].
     PKZO_EXPORT glm::vec4 rgba(unsigned int r, unsigned int g, unsigned int b, unsigned int a);
 
-    //! @brief Convert RGB color from separate float components to glm::vec3.
+    //! Convert RGB color from separate float components to glm::vec3.
     //!
     //! @param r Red component, in [0.0, 1.0].
     //! @param g Green component, in [0.0, 1.0].
@@ -63,7 +89,7 @@ namespace pkzo::three
     //! @return The color as a glm::vec3, with each channel in [0.0, 1.0].
     PKZO_EXPORT glm::vec3 rgb(float r, float g, float b);
 
-    //! @brief Convert RGBA color from separate float components to glm::vec4.
+    //! Convert RGBA color from separate float components to glm::vec4.
     //!
     //! @param r Red component, in [0.0, 1.0].
     //! @param g Green component, in [0.0, 1.0].
@@ -71,4 +97,135 @@ namespace pkzo::three
     //! @param a Alpha component, in [0.0, 1.0].
     //! @return The color as a glm::vec4, with each channel in [0.0, 1.0].
     PKZO_EXPORT glm::vec4 rgba(float r, float g, float b, float a);
+
+    //! PBR Material class.
+    class PKZO_EXPORT Material
+    {
+    public:
+        Material() = default;
+        Material(const Material&) = default;
+        Material(Material&&) = default;
+
+        Material(const glm::vec3& base_color_factor,
+                 const std::shared_ptr<Texture>& base_color_map,
+                 const std::shared_ptr<Texture>& metallic_roughness_map,
+                 const std::shared_ptr<Texture>& normal_map,
+                 const std::shared_ptr<Texture>& occlusion_map,
+                 const glm::vec3& emissive_factor,
+                 const std::shared_ptr<Texture>& emissive_map);
+
+        ~Material() = default;
+
+        Material& operator=(const Material&) = default;
+        Material& operator=(Material&&) = default;
+
+        //! Gets the base color factor.
+        //!
+        //! @return The base color factor.
+        glm::vec3 get_base_color_factor() const;
+
+        //! Sets the base color factor.
+        //! @param color The new base color factor.
+        void set_base_color_factor(const glm::vec3& color);
+
+        //! Gets the emissive factor.
+        //!
+        //! @return The emissive factor.
+        glm::vec3 get_emissive_factor() const;
+
+        //! Sets the emissive factor.
+        //!
+        //! @param factor The new emissive factor.
+        void set_emissive_factor(const glm::vec3& factor);
+
+        //! Gets the base color texture.
+        //!
+        //! @return The base color texture.
+        std::shared_ptr<Texture> get_base_color_map() const;
+
+        //! Sets the base color texture.
+        //! @param texture The new base color texture.
+        void set_base_color_map(const std::shared_ptr<Texture>& value);
+
+        //! Gets the metallic roughness texture.
+        //!
+        //! @return The metallic roughness texture.
+        std::shared_ptr<Texture> get_metallic_roughness_map() const;
+
+        //! Sets the metallic roughness texture.
+        //!
+        //! @param texture The new metallic roughness texture.
+        void set_metallic_roughness_map(const std::shared_ptr<Texture>& value);
+
+        //! Gets the normal texture.
+        //!
+        //! @return The normal texture.
+        std::shared_ptr<Texture> get_normal_map() const;
+
+        //! Sets the normal texture.
+        //! @param texture The new normal texture.
+        void set_normal_map(const std::shared_ptr<Texture>& value);
+
+        //! Gets the occlusion texture.
+        //!
+        //! @return The occlusion texture.
+        std::shared_ptr<Texture> get_occlusion_map() const;
+
+        //! Sets the occlusion texture.
+        //!
+        //! @param texture The new occlusion texture.
+        void set_occlusion_map(const std::shared_ptr<Texture>& value);
+
+        //! Gets the emissive texture.
+        //!
+        //! @return The emissive texture.
+        std::shared_ptr<Texture> get_emissive_map() const;
+
+        //! Sets the emissive texture.
+        //!
+        //! @param texture The new emissive texture.
+        void set_emissive_map(const std::shared_ptr<Texture>& value);
+
+    private:
+        glm::vec3                base_color_factor      = {1.0f, 1.0f, 1.0f};
+        glm::vec3                emissive_factor        = {0.0f, 0.0f, 0.0f};
+        std::shared_ptr<Texture> base_color_map         = get_white_texture();
+        std::shared_ptr<Texture> metallic_roughness_map = get_black_texture();
+        std::shared_ptr<Texture> normal_map             = get_normal_texture();
+        std::shared_ptr<Texture> occlusion_map          = get_black_texture();
+        std::shared_ptr<Texture> emissive_map           = get_black_texture();
+    };
+
+    //! Creates an opaque material with a base color.
+    //!
+    //! @param base_color A glm::vec3 representing the base RGB color.
+    //! @return A std::shared_ptr<Material> of the created Material object.
+    PKZO_EXPORT std::shared_ptr<Material> create_opaque_material(const glm::vec3& base_color);
+
+    //! Creates an opaque material with textures for base color and metallic-roughness.
+    //!
+    //! @param base_color Shared pointer to the base color texture.
+    //! @param metallic_roughness Shared pointer to the metallic-roughness texture.
+    //! @return A std::shared_ptr<Material> of the created Material object.
+    PKZO_EXPORT std::shared_ptr<Material> create_opaque_material(const std::shared_ptr<Texture>& base_color, const std::shared_ptr<Texture>& metallic_roughness);
+
+    //! Creates an opaque material with additional normal texture.
+    //!
+    //! @param base_color Shared pointer to the base color texture.
+    //! @param metallic_roughness Shared pointer to the metallic-roughness texture.
+    //! @param normal Shared pointer to the normal texture.
+    //! @return A std::shared_ptr<Material> of the created Material object.
+    PKZO_EXPORT std::shared_ptr<Material> create_opaque_material(const std::shared_ptr<Texture>& base_color, const std::shared_ptr<Texture>& metallic_roughness, const std::shared_ptr<Texture>& normal);
+
+    //! Creates an emissive material with an RGB factor.
+    //!
+    //! @param factor A glm::vec3 representing the emission factor.
+    //! @return A std::shared_ptr<Material> of the created Material object.
+    PKZO_EXPORT std::shared_ptr<Material> create_emissive_material(const glm::vec3& factor);
+
+    //! Creates an emissive material with a texture map.
+    //!
+    //! @param map Shared pointer to the emissive texture map.
+    //! @return A std::shared_ptr<Material> of the created Material object.
+    PKZO_EXPORT std::shared_ptr<Material> create_emissive_material(const std::shared_ptr<Texture>& map);
 }
